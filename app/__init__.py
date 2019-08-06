91be202426390c0d9bbd9c0845d643a1297a5749
mrbrunotte <mrbrunotte@gmail.com>
2019-08-06 10:06:25 +0200

    Lesson 1 complete

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app import routes # must be below app=flask(__name__)
+from app import routes              # must be below app=flask(__name__)
